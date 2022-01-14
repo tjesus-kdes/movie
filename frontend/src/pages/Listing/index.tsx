b51bfb1db15b7f8ff3c13c0424e4227d568fcb4d
tjesus-kdes <tjesuskdes@gmail.com>
2022-01-14 18:16:07 -0300

    Customizacao do MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,38 @@
 import Pagination from "components/Pagination";
+//import MovieStars from "components/MovieStars";
+//import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
-    return(
-        <Pagination />
+    return (
+        <>
+            <Pagination />
 
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+
+            </div>
+
+        </>
     );
 
 }
