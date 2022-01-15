4239f21540276ebc985a88c9d0d590413b117ec5
tjesus-kdes <tjesuskdes@gmail.com>
2022-01-15 17:47:11 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -1,7 +1,14 @@
 package com.devsuperior.dsmovie.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
 public class Score {
 
+	@EmbeddedId
 	private ScorePK id = new ScorePK();
 	private Double value;
 	
--- backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -1,7 +1,17 @@
 package com.devsuperior.dsmovie.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
 	
