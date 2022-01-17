82c77f4c1f5099596ec19e03c497165a8439172c
tjesus-kdes <tjesuskdes@gmail.com>
2022-01-16 22:34:03 -0300

    correcao classe MovieCard - 10:15:52 PM: src/components/MovieCard/index.tsx 10:15:52 PM:   Line 21:27:  Unexpected template string expression  no-template-curly-in-string

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,5 +1,6 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 function MovieCard() {
 
@@ -11,23 +12,32 @@ function MovieCard() {
         score: 4.5
     };
 
-    return (
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore />
-
-                <Link to={'/form/${movie.id}'}>
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-                </Link>
-
+  <div className="dsmovie-form-container">
+    <img className="dsmovie-movie-card-image" src="url" alt="The Witcher" />
+    <div className="dsmovie-card-bottom-container">
+        <h3>"The Witcher"</h3>
+        <form className="dsmovie-form">
+            <div className="form-group dsmovie-form-group">
+                <label htmlFor="email">Informe seu email</label>
+                <input type="email" className="form-control" id="email" />
             </div>
-        </div>
-
-
-
-    );
+            <div className="form-group dsmovie-form-group">
+                <label htmlFor="score">Informe sua avaliação</label>
+                <select className="form-control" id="score">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div className="dsmovie-form-btn-container">
+                <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+            </div>
+        </form >
+        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+    </div >
+</div >
 }
 
 export default MovieCard;
